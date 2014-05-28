737857bbb5979ce48e066787e195530bae5391d8
SoInna <SoInna@users.noreply.github.com>
2014-05-29 01:50:04 +0600

    Update script.js

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ function init() {
 		var x = Number(row_data[10]);
 		var y = Number(row_data[9]);
 		var text = row_data[0];
-        var size = Number(row_data[7]) * 10;
+        var size = Number(row_data[7]) * 10 + 10;
         
         var placemark = new ymaps.Placemark([x, y], {
 			content: text,
